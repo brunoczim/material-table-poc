abcda5bac53d2dd38f1ad5dd8f12902dd25d11d9
brunoczim <brunoczim@gmail.com>
2022-06-21 18:10:10 -0300

    initial version

---
+++ src/data.ts
@@ -0,0 +1,243 @@
+export interface Row {
+  name: string;
+  idNum: string;
+  due: string;
+  status: string;
+}
+
+export const ROWS: Row[] = [
+  {
+    "name": "Lakisha Dejesus",
+    "idNum": "62b1d22d3ea773b1937b6105",
+    "due": "06-09-2019 03:52",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Clements Roach",
+    "idNum": "62b1d22d70589554da0d43c4",
+    "due": "15-12-2021 08:03",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Evangelina Perkins",
+    "idNum": "62b1d22d59548769a0cae471",
+    "due": "09-08-2014 10:46",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Desiree Morales",
+    "idNum": "62b1d22ddf0caef3fb090897",
+    "due": "07-01-2014 01:45",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Cleveland Duffy",
+    "idNum": "62b1d22d599c4e56f1a78db8",
+    "due": "29-03-2018 12:22",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Janell Paul",
+    "idNum": "62b1d22dde3ae1c762315f5b",
+    "due": "15-09-2017 01:29",
+    "status": "ERRADO"
+  },
+  {
+    "name": "Alexandra Newton",
+    "idNum": "62b1d22d2f66c591783e6d4f",
+    "due": "09-05-2015 08:33",
+    "status": "ERRADO"
+  },
+  {
+    "name": "Wade Warren",
+    "idNum": "62b1d22d10438b3705d3ad7f",
+    "due": "30-03-2020 10:32",
+    "status": "FEITO"
+  },
+  {
+    "name": "Judith Burgess",
+    "idNum": "62b1d22d36d31fd4b6f8752d",
+    "due": "14-12-2018 07:05",
+    "status": "ERRADO"
+  },
+  {
+    "name": "Maureen Terrell",
+    "idNum": "62b1d22dfeb6bbd698bab0fe",
+    "due": "12-04-2020 01:06",
+    "status": "SEILA"
+  },
+  {
+    "name": "Brown Simmons",
+    "idNum": "62b1d22dabbfe70e8df64484",
+    "due": "15-07-2018 10:40",
+    "status": "FEITO"
+  },
+  {
+    "name": "Cynthia Vang",
+    "idNum": "62b1d22d1625efe666e62388",
+    "due": "25-12-2014 03:23",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Clemons Hampton",
+    "idNum": "62b1d22d75b71f32dce613a8",
+    "due": "26-07-2016 01:04",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Holden Doyle",
+    "idNum": "62b1d22d2c6622ec2bccd028",
+    "due": "21-10-2017 09:01",
+    "status": "SEILA"
+  },
+  {
+    "name": "Therese Parks",
+    "idNum": "62b1d22d58bb310d4f3f1af9",
+    "due": "13-10-2016 02:11",
+    "status": "FEITO"
+  },
+  {
+    "name": "Knapp Ford",
+    "idNum": "62b1d22dc4af1f670b7b4466",
+    "due": "20-05-2017 08:03",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Sharron Pace",
+    "idNum": "62b1d22d381842bfe6d59d18",
+    "due": "31-01-2016 06:51",
+    "status": "FEITO"
+  },
+  {
+    "name": "Justine Mosley",
+    "idNum": "62b1d22d0dd40a5351dc45f5",
+    "due": "26-06-2018 05:36",
+    "status": "FEITO"
+  },
+  {
+    "name": "Ward Strong",
+    "idNum": "62b1d22d34e717859eceade8",
+    "due": "06-02-2022 03:55",
+    "status": "FEITO"
+  },
+  {
+    "name": "Combs Church",
+    "idNum": "62b1d22d1be0a345ac868c78",
+    "due": "15-09-2017 01:32",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Petra Delacruz",
+    "idNum": "62b1d22d446858ea901c67af",
+    "due": "22-10-2016 07:26",
+    "status": "SEILA"
+  },
+  {
+    "name": "Dolly Mccormick",
+    "idNum": "62b1d22d72714e105a01b011",
+    "due": "03-03-2021 03:33",
+    "status": "FEITO"
+  },
+  {
+    "name": "Freida Sweet",
+    "idNum": "62b1d22d960d7619decabfd7",
+    "due": "11-03-2015 05:53",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Johns Aguilar",
+    "idNum": "62b1d22deda6f63fbc513b91",
+    "due": "26-05-2014 03:07",
+    "status": "ERRADO"
+  },
+  {
+    "name": "Kellie Mcmillan",
+    "idNum": "62b1d22d0a779a67c007058d",
+    "due": "03-10-2017 02:24",
+    "status": "SEILA"
+  },
+  {
+    "name": "Gilbert Valencia",
+    "idNum": "62b1d22df087299a369aa9b0",
+    "due": "19-07-2020 03:12",
+    "status": "FEITO"
+  },
+  {
+    "name": "Whitehead Chapman",
+    "idNum": "62b1d22d673ba24c7941e7ae",
+    "due": "25-02-2019 04:50",
+    "status": "FEITO"
+  },
+  {
+    "name": "Diane Holcomb",
+    "idNum": "62b1d22d9a85f8f9435fb3ae",
+    "due": "04-12-2016 08:19",
+    "status": "FEITO"
+  },
+  {
+    "name": "Simone Guzman",
+    "idNum": "62b1d22d9f9961c8c9b976ae",
+    "due": "14-04-2019 05:13",
+    "status": "ERRADO"
+  },
+  {
+    "name": "Morin Valentine",
+    "idNum": "62b1d22de8e11fc0236cf40b",
+    "due": "27-08-2018 11:01",
+    "status": "FEITO"
+  },
+  {
+    "name": "Sheena Collier",
+    "idNum": "62b1d22dd58698c94cfbeaaf",
+    "due": "03-12-2021 10:48",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Morse Stanton",
+    "idNum": "62b1d22dd5cb02dba98a5c2c",
+    "due": "21-06-2022 10:04",
+    "status": "FEITO"
+  },
+  {
+    "name": "Bond Wooten",
+    "idNum": "62b1d22d5f80efc568788ef3",
+    "due": "04-08-2020 09:06",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Fulton Contreras",
+    "idNum": "62b1d22d5bf364267d5c0d67",
+    "due": "13-03-2014 11:50",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Beard Dunlap",
+    "idNum": "62b1d22ddb29703e6e85acc4",
+    "due": "28-06-2021 05:35",
+    "status": "NAO USAR"
+  },
+  {
+    "name": "Holder Wyatt",
+    "idNum": "62b1d22da88b168bb68f857e",
+    "due": "28-09-2017 06:03",
+    "status": "ERRADO"
+  },
+  {
+    "name": "Norton Ellis",
+    "idNum": "62b1d22d4a904a2c7efaf59c",
+    "due": "11-02-2022 02:10",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Victoria Reeves",
+    "idNum": "62b1d22ddb336eb3ea20ec5b",
+    "due": "02-06-2018 02:49",
+    "status": "EM ANALISE"
+  },
+  {
+    "name": "Amparo Bridges",
+    "idNum": "62b1d22d76e5ac29a6e3b422",
+    "due": "01-01-2022 04:38",
+    "status": "ERRADO"
+  }
+];
